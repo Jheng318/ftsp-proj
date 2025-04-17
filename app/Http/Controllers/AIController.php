966b75ccc8283f7076308fe8b9876b701a2f21ff
Jheng318 <jhchan0514@gmail.com>
2025-04-17 15:27:18 +0800

    some useful codes

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -4,14 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Amrachraf6699\LaravelGeminiAi\Facades\GeminiAi;
+use Illuminate\Support\Facades\Storage;
+use Spatie\PdfToText\Pdf;
 
 class AIController extends Controller
 {
     // Using additional options
     public function index(){
 
-        $response = GeminiAi::generateText("Tell me about black holes.");
-        echo $response; // Outputs just the text
-
+        $student = [
+            [
+                "name" => "john",
+                "interest" => "web development",
+                "frameworks" => ["react", "CSS", "HTML"],
+                "language" => ["Javascript", "Python"],
+                "gpa" => 3.7,
+            ],
+            [
+                "name" => "jane",
+                "interest" => "automation",
+                "framework" => "UiPath",
+                "language" => null,
+                "gpa" => 3.00,
+            ]
+        ];
+        $job = [
+            [
+                'job role' => 'Front-End Developer',
+                'job requirement' => [
+                    "gpa" => "3.5 and above",
+                    "framework" => "React",
+                    "language" => "Javascript"
+                ],
+            ],
+            [
+                'job role' => 'RPA automation',
+                'job requirement' => [
+                    "gpa" => null,
+                    "framework" => "UiPath",
+                    "language" => null
+                ],
+            ],
+        ];
+        $prompt = "Given the following student data and job description, match the student with the most suitable job. Just show the student and the job allocation with no explanation.\n\n";
+        $prompt .= "Students: " . json_encode($student, JSON_PRETTY_PRINT) . "\n";
+        $prompt .= "Job: " . json_encode($job, JSON_PRETTY_PRINT) . "\n";
+        $response = GeminiAi::generateText($prompt, ["model" => "gemini-2.0-flash-lite"]);
+        dd($response);
+        //echo $response; // Outputs just the text
+    }
+    public function analyze(){
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\AIController;
+use App\Http\Controllers\FileController;
+use Carbon\TranslatorStrongTypeInterface;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
 
 Route::inertia('/', 'welcome');
-Route::get('/ai', [AIController::class, 'index']);
+Route::get('/genText', [AIController::class, 'index']);
+Route::get('/analyzefiles', [AIController::class, 'analyze']);
+Route::inertia('/file', 'upload');
