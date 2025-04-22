9818e0b7519aebc16fd94fa9b7963840180e0f7e
Jcling <jcling.sg@yahoo.com>
2025-04-22 12:00:10 +0800

    tiny progress on the student home page

--- resources/js/Pages/Student/Main.jsx
+++ resources/js/Pages/Student/Main.jsx
@@ -1,8 +1,9 @@
 import "@/css/Student/main.css";
+import background from "@/images/background.jpg";
 function Main() {
     return (
         <>
-            <section id="hero">
+            <section id="hero" style={{backgroundImage: `url(${background})`}}>
                 <h1>
                     Internship/PRISIM Project <br />
                     Interest Mapping <br />
