c9ed3cd643e6d8ddd2522809fa44f34d21eb8f63
Jheng318 <jhchan0514@gmail.com>
2025-04-24 11:06:12 +0800

    changed the gpa requirement to gpa constraints in the prisim table

--- database/migrations/2025_04_17_073059_create_prisims_table.php
+++ database/migrations/2025_04_17_073059_create_prisims_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('description');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->decimal('gpa_requirenment', 3, 2);
+            $table->string('gpa_constraints');
             $table->integer('no_of_students');
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
