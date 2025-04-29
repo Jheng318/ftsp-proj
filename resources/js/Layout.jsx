d48a4b11178368cf4e4f82d9f98490a695d3d51a
Jcling <jcling.sg@yahoo.com>
2025-04-29 18:00:07 +0800

    updated code

--- resources/js/Layout.jsx
+++ resources/js/Layout.jsx
@@ -47,11 +47,7 @@ export default function Layout({ children }) {
                                     Internship
                                 </Nav.Link>
                                 <Nav.Link href="/ftsp-proj/prism-student">
-<<<<<<< Updated upstream
-                                    PRISM
-=======
                                     PRISIM
->>>>>>> Stashed changes
                                 </Nav.Link>
                                 <Nav.Link href="/ftsp-proj/allocation-student">
                                     Allocation
--- resources/js/Pages/Student/Main.jsx
+++ resources/js/Pages/Student/Main.jsx
@@ -34,19 +34,11 @@ function Main({ internships, prism_projects, allocation}) {
                     <p>In order for successful allocation, you will need to submit a form depending on which allocation and indicate your skills and strengths.</p>
                     <p>If you have submitted either of the interest forms, you can also edit your interests by clicking the respective links below.</p>
                     <div className="d-flex">
-<<<<<<< Updated upstream
-                        <div className="detail-btn right">
-                            <a href="/prism-proj/intern-student">Internship</a>
-                        </div>
-                        <div className="detail-btn">
-                            <a href="/prism-proj/intern-student">PRISM</a>
-=======
                         <div className="detail-btn">
                             <a href="/prism-proj/intern-student">View More</a>
                         </div>
                         <div className="detail-btn">
                             <a href="/prism-proj/intern-student">View More</a>
->>>>>>> Stashed changes
                         </div>
                     </div>
                 </div>
@@ -57,67 +49,15 @@ function Main({ internships, prism_projects, allocation}) {
 
             <div className="d-flex justify-content-between align-items-center title">
                 <h2>Posted Internships</h2>
-<<<<<<< Updated upstream
-                <CardButton btnColor="#6393F2">
-                    <img src={sortIcon} alt="sortIcon" />
-                    Sort By
-=======
                 <CardButton
                     btnColor="#6393F2"
                 >
                     <img src={sortIcon} alt="sortIcon" />Sort By
->>>>>>> Stashed changes
                 </CardButton>
             </div>
 
             <section id="all-internships">
                 {internships.map((internship) => {
-<<<<<<< Updated upstream
-                    const {
-                        id,
-                        title,
-                        created_at,
-                        description,
-                        company_name,
-                        salary,
-                        user_name,
-                    } = internship;
-                    const editedDesc = description.split(".");
-
-                    return (
-                        <a
-                            href={`/ftsp-proj/intern-student/${id}`}
-                            key={id}
-                            style={{ textDecoration: "none" }}
-                        >
-                            <Card style={{ width: "100%", cursor: "pointer" }}>
-                                <Card.Body>
-                                    <Card.Title>{title}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted d-flex align-items-center">
-                                        <span>Posted {created_at}</span>
-                                        <span className="dot">&#x2022;</span>
-                                        {company_name}
-                                    </Card.Subtitle>
-                                    <Card.Text>{editedDesc[0]}.</Card.Text>
-                                    <section
-                                        className="d-flex align-items-center"
-                                        id="contact_salary"
-                                    >
-                                        <div>
-                                            <img
-                                                src={personIcon}
-                                                alt="personIcon"
-                                            />
-                                            {user_name}
-                                        </div>
-                                        <div>
-                                            <img
-                                                src={salaryIcon}
-                                                alt="salaryIcon"
-                                            />
-                                            ${salary}
-                                        </div>
-=======
                     const { id, title, created_at, description, company_name, salary, user_name } = internship;
                     const editedDesc = description.split('.');
 
@@ -140,7 +80,6 @@ function Main({ internships, prism_projects, allocation}) {
                                             ${salary}
                                         </div>
 
->>>>>>> Stashed changes
                                     </section>
                                 </Card.Body>
                             </Card>
@@ -155,59 +94,15 @@ function Main({ internships, prism_projects, allocation}) {
 
             <div className="d-flex justify-content-between align-items-center title">
                 <h2>Posted PRISM Projects</h2>
-<<<<<<< Updated upstream
-                <CardButton btnColor="#6393F2">
-                    <img src={sortIcon} alt="sortIcon" />
-                    Sort By
-=======
                 <CardButton
                     btnColor="#6393F2"
                 >
                     <img src={sortIcon} alt="sortIcon" />Sort By
->>>>>>> Stashed changes
                 </CardButton>
             </div>
 
             <section id="all-prism">
                 {prism_projects.map((project) => {
-<<<<<<< Updated upstream
-                    const {
-                        id,
-                        title,
-                        created_at,
-                        description,
-                        type,
-                        user_name,
-                    } = project;
-                    const editedDesc = description.split(".");
-
-                    return (
-                        <a
-                            href={`/ftsp-proj/prism-student/${id}`}
-                            key={id}
-                            style={{ textDecoration: "none" }}
-                        >
-                            <Card style={{ width: "100%", cursor: "pointer" }}>
-                                <Card.Body>
-                                    <Card.Title>{title}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted d-flex align-items-center">
-                                        <span>Posted {created_at}</span>
-                                        <span className="dot">&#x2022;</span>
-                                        {type}
-                                    </Card.Subtitle>
-                                    <Card.Text>{editedDesc[0]}.</Card.Text>
-                                    <section
-                                        className="d-flex align-items-center"
-                                        id="contact_salary"
-                                    >
-                                        <div>
-                                            <img
-                                                src={personIcon}
-                                                alt="personIcon"
-                                            />
-                                            {user_name}
-                                        </div>
-=======
                     const { id, title, created_at, description, type, user_name } = project;
                     const editedDesc = description.split('.');
 
@@ -226,7 +121,6 @@ function Main({ internships, prism_projects, allocation}) {
                                             {user_name}
                                         </div>
 
->>>>>>> Stashed changes
                                     </section>
                                 </Card.Body>
                             </Card>
@@ -238,17 +132,6 @@ function Main({ internships, prism_projects, allocation}) {
             <div className="view-btn">
                 <a href="/prism-proj/intern-student">View More</a>
             </div>
-<<<<<<< Updated upstream
-
-            <div className="title">
-                <h2>Allocation Status</h2>
-            </div>
-
-            <section id="interests">
-                {allocation.allocation_status == false && <div>Test</div>}
-            </section>
-=======
->>>>>>> Stashed changes
         </>
     );
 }
